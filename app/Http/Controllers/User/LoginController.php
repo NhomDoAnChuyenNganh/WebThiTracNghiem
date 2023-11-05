38aea3905c627fb292db95707d3237591bbedfa7
HauNui123 <vanhau98.nhd@gmail.com>
2023-11-05 15:11:23 +0700

    trangLogin
    chỉ thiết kế

---
+++ app/Http/Controllers/User/LoginController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('user.login',[
+            'title'=>'Đăng Nhập Hệ Thống'
+        ]);
+    }
+}
