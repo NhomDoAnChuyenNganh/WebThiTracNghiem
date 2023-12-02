08870feca2a8a555cde9bae3460afea5872764b6
Nhat Tran <nhat7858@gmail.com>
2023-12-02 20:20:32 +0700

    update

--- resources/views/gv_soande/soan-de.blade.php
+++ resources/views/gv_soande/soan-de.blade.php
@@ -6,42 +6,42 @@
 
 @section('content')
 @if (session('error'))
-        <div class="alert alert-danger">
-            {{ session('error') }}
-        </div>
-    @endif
-    @if (session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
+<div class="alert alert-danger">
+    {{ session('error') }}
+</div>
+@endif
+@if (session('success'))
+<div class="alert alert-success">
+    {{ session('success') }}
+</div>
+@endif
 <div class="noidung" style="height: 1000px; width: 800px; background-color: white;margin: auto;">
     <div class="container">
         <h1>{{ $title }}</h1>
         <h2>Danh sách đề thi</h2>
 
-    <table class="table" style="background-color: aliceblue; margin-top: 20px;">
-        <thead>
-            <tr>
-                <th>Môn Học</th>
-                <th>Tên Đề Thi</th>
-                <th>Số Lượng Câu Hỏi</th>
-                <th >Thao Tác</th>
-            </tr>
-        </thead>
-        <tbody id="chuongs-table">
-            @foreach ($dethis as $dethi)
+        <table class="table" style="background-color: aliceblue; margin-top: 20px;">
+            <thead>
+                <tr>
+                    <th>Môn Học</th>
+                    <th>Tên Đề Thi</th>
+                    <th>Số Lượng Câu Hỏi</th>
+                    <th>Thao Tác</th>
+                </tr>
+            </thead>
+            <tbody id="chuongs-table">
+                @foreach ($dethis as $dethi)
                 <tr>
                     <td>{{ optional($dethi->MonHoc)->TenMH }}</td>
                     <td>{{ $dethi->TenDeThi }}</td>
                     <td style="text-align: center;">{{ $dethi->SoLuongCH }}</td>
                     <td>
-                        <a href="{{ route('sua-de-thi', ['id' => $dethi->MaDe]) }}" class="btn btn-warning">Sửa</a>              
+                        <a href="{{ route('sua-de-thi', ['id' => $dethi->MaDe]) }}" class="btn btn-warning">Soạn đề</a>
                     </td>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
-@endsection
+    @endsection
